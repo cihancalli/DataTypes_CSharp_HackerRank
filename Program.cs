8c762ee36a8f6c88da5ceb8cc48ee4232e61baf4
Cihan ÇALLI <cihan.callii@gmail.com>
2022-03-31 13:04:37 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            // Declare second integer, double, and String variables.
+            int i1;
+            double d1;
+            string s1;
+            
+            // Read and save an integer, double, and String to your variables.
+            i1 = int.Parse(Console.ReadLine());
+            d1 = double.Parse(Console.ReadLine());
+            s1 = Console.ReadLine();
+            
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(i + i1);
+            
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine((d + d1).ToString("F1"));
+            
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s + s1);
+        }
+    }
+}
